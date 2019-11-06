e4c4f638a6d3f3d7119855d0ac68a3a908589cdb
Alejandro-PC\Alejandro <>
2019-11-06 17:25:59 +0100

    upgrade transform

--- Engine/ComponentTransform.cpp
+++ Engine/ComponentTransform.cpp
@@ -5,14 +5,17 @@
 ComponentTransform::ComponentTransform(GameObject* _game_object) : Component(_game_object, ComponentType::TRANSFORM)
 {
 	position = { 0.0f, 0.0f, 0.0f };
-	rotation = { 0.0f, 0.0f, 0.0f, 0.0f };
+	rotation = { 0.0f, 0.0f, 0.0f, 1.0f };
 	euler = { 0.0f, 0.0f, 0.0f };
 	scale = { 1.0f, 1.0f, 1.0f };
 
 	local_position = { 0.0f, 0.0f, 0.0f };
-	local_rotation = { 0.0f, 0.0f, 0.0f, 0.0f };
+	local_rotation = { 0.0f, 0.0f, 0.0f, 1.0f };
 	local_euler = { 0.0f, 0.0f, 0.0f };
 	local_scale = { 1.0f, 1.0f, 1.0f };
+
+	local_transform_matrix = float4x4::FromTRS(local_position, local_rotation, local_scale);
+	transform_matrix = float4x4::FromTRS(position, rotation, scale);
 }
 
 
@@ -32,20 +35,19 @@ float4x4 ComponentTransform::GetTransformMatrix()
 
 	if (GetMyGameObject()->GetParent())
 	{
-		transform_matrix = GetMyGameObject()->GetParent()->GetTransform()->GetTransformMatrix() * local_transform_matrix;
-		/*if (GetMyGameObject()->GetChildren().size() > 0)
-		{
-			for (int i = 0; i < GetMyGameObject()->GetChildren().size(); i++)
-			{
-				GetMyGameObject()->GetChildren()[i]->GetTransform()->GetTransformMatrix();
-			}
-		}*/
+		//std::string a = GetMyGameObject()->GetName();
+		//int x = GetMyGameObject()->GetChildren().size();
+		transform_matrix = GetMyGameObject()->GetParent()->GetTransform()->transform_matrix * local_transform_matrix;
 	}
 	else
 	{
 		transform_matrix = local_transform_matrix;
 	}
 
+	for (int i = 0; i < GetMyGameObject()->GetChildren().size(); i++)
+	{
+		GetMyGameObject()->GetChildren()[i]->GetTransform()->GetTransformMatrix();
+	}
 
 	transform_matrix.Decompose(position, rotation, scale);
 	return transform_matrix;
